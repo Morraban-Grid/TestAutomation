f8036c460cd03a12cdea661d96549b2fb92b4d21
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-05-12 11:37:01 -0500

    feat: add new features for page objetcs

--- TestAutomation.Tests/PageObjectPattern/PageObject/HomePage/HomePageObject.cs
+++ TestAutomation.Tests/PageObjectPattern/PageObject/HomePage/HomePageObject.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Tests.PageObjectPattern.Models;
 
 namespace TestAutomation.Tests.PageObjectPattern.PageObject.HomePage
 {
     public class HomePageObject
     {
+        //definiendo el driver
+        //para las frutas que seran una lista
+
+        private readonly IWebDriver driver; 
+        private List<IWebElement> DisplayedFruits => driver.FindElements(By.ClassName("fruit")).Where(fruit => fruit.Displayed).ToList();
     }
 }
