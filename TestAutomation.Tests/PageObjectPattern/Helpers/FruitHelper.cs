7c8db2d869144404ed15bab653e5d573789b21cb
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-05-12 13:44:47 -0500

    feat: add new features in FrutiHelper class

--- TestAutomation.Tests/PageObjectPattern/Helpers/FruitHelper.cs
+++ TestAutomation.Tests/PageObjectPattern/Helpers/FruitHelper.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAutomation.Tests.PageObjectPattern.Models;
+using TestAutomation.Tests.PageObjectPattern.PageObject.HomePage;
 
 namespace TestAutomation.Tests.PageObjectPattern.Helpers
 {
-    internal class FruitHelper
+    public static class FruitHelper
     {
+        public static IList<FruitWebElement> Parse(IList<IWebElement> fruits)
+        {
+            return fruits.Select(fruit => new FruitWebElement(fruit)).ToList();
+        }
+        //Método que nos retorme un valor de tipo FrutiModel
+        public static IList<FruitModel> Parse(IList<FruitWebElement> fruits)
+        {
+            return fruits.Select(fruit => Parse(fruit)).ToList();
+        }
+        //Método para convertir y separar el precio y convertir a decimal
+        public static FruitModel Parse(FruitWebElement element)
+        {
+            var price = decimal.Parse(element.Price.Split(' ')[0]);
+            return new FruitModel(element.Name, price, element.Description);
+        }
     }
 }
