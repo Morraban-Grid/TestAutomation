69759939528d0c36b3899d9fd7e3e26bb222e18d
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-05-12 11:30:01 -0500

    feat: add new HomePageObject class

---
+++ TestAutomation.Tests/PageObjectPattern/PageObject/HomePage/HomePageObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomation.Tests.PageObjectPattern.PageObject.HomePage
+{
+    public class HomePageObject
+    {
+    }
+}
