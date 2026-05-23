daeb16875ee26a60b0254e02721804e67a29b3c6
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-05-22 19:23:57 -0500

    add new CartItemWebElement class

---
+++ TestAutomation.Tests/PageObjectPattern/PageObject/ShoppingCart/CartItemWebElement.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+namespace TestAutomation.Tests.PageObjectPattern.PageObject.ShoppingCart
+{
+    public class CartItemWebElement
+    {
+        private IWebElement element;
+        private IWebElement ButtonRemove => element.FindElement(By.CssSelector("button"));
+        private IWebElement InfoText => element.FindElement(By.CssSelector("span"));
+        private IWebElement InputFieldQuantity => element.FindElement(By.CssSelector("input"));
+        public CartItemWebElement(IWebElement element)
+        {
+            this.element = element;
+        }
+        public void ClickButtonRemove() => ButtonRemove.Click();
+        public string GetText() => InfoText.Text;
+        public void InputQuantity(int quantity)
+        {
+            InputFieldQuantity.Clear();
+            InputFieldQuantity.SendKeys(quantity.ToString());
+        }
+    }
+}
--- TestAutomation.Tests/PageObjectPattern/PageObject/ShoppingCart/ShoppingCartPageObject.cs
+++ TestAutomation.Tests/PageObjectPattern/PageObject/ShoppingCart/ShoppingCartPageObject.cs
@@ -9,9 +9,14 @@ namespace TestAutomation.Tests.PageObjectPattern.PageObject.ShoppingCart
     public class ShoppingCartPageObject
     {
         private readonly IWebDriver driver;
+        private IWebElement TotalPrice => driver.FindElement(By.Id("totalPrice"));
+        private IWebElement ButtonClose => driver.FindElement(By.Id("CloseCart"));
         public ShoppingCartPageObject(IWebDriver driver)
         {
             this.driver = driver;
         }
+        public void ClickButtonClose() => ButtonClose.Click();
+        public decimal GetTotalPrice() => decimal.Parse(TotalPrice.Text);
+        private List<IWebElement> CartItems => driver.FindElements(By.ClassName("cartitem")).ToList();
     }
 }
